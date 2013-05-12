e04e1f518ad378c509986df5aa4ba726f05ba8a2
BlahGeek <BlahGeek@Gmail.com>
2013-05-12 19:49:37 +0800

    add views for calendar

--- weixin/views.py
+++ weixin/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import render_to_response, redirect
 from django.contrib import messages
 from .utils import *
 from .messages import *
+from calendar.sample import getTodayEvents, getTomorrowEvents
 
 def index(req):
     if not checkSig(req):
@@ -20,6 +21,28 @@ def index(req):
     if req.method == 'GET':
         return HttpResponse(req.GET.get('echostr', ''))
     req_msg = parseXml(req)
-    return HttpResponse(makeImageMsg(req_msg, 
-        (('https://encrypted.google.com/images/srpr/logo4w.png', 'Google', 'Test', 'https://google.com'), )))
+    ret = u''
+
+    if req_msg.get('MsgType', '') == 'text':
+        text = req_msg.get('Content', '')
+        if u'亲爱' in text and u'有空' in text:
+            events = None
+            if u'今天' in text:
+                events = getTodayEvents()
+            elif u'明天' in text:
+                events = getTomorrowEvents()
+
+            if events is None:
+                ret = u'我是机器人，理解不了了耶，直接来找我问啦...'
+            elif len(events) == 0:
+                ret = u'似乎没有事耶 :P'
+            else:
+                for x in events:
+                    ret += x[2].strftime('%H:%M') + '-'
+                    ret += x[3].strftime('%H:%M')
+                    if x[1] != '':
+                        ret += u'在' + x[1]
+                    ret += u'有' + x[0] + u'; '
+
+    return HttpResponse(makeTextMsg(req_msg, ret))
 
