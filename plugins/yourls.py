1ab2de079bfef9df1d5ada87d011bc0786e9fcfa
BlahGeek <blahgeek@gmail.com>
2015-01-18 10:47:16 +0800

    better shorter

--- plugins/yourls.py
+++ plugins/yourls.py
@@ -4,18 +4,24 @@
 
 from . import Plugin
 import requests
+import re
 
 class YOURLSPlugin(Plugin):
 
-    target = None
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
     signature = 'f4531f65ca'
     url = 'http://blaa.ml/s/yourls-api.php'
 
     def predict(self, text):
-        action, _, self.target = text.strip().partition(' ')
-        if action == 'short' and len(self.target) > 0:
-            return 100
+        if self.regex.match(text):
+            return 90
         return 0
 
     def handle(self, text, userid):
@@ -23,14 +29,13 @@ class YOURLSPlugin(Plugin):
             r = requests.get(self.url, params={
                                 'action': 'shorturl',
                                 'signature': self.signature,
-                                'url': self.target,
+                                'url': text,
                                 'format': 'json'
                              }, timeout=5).json()
             if not 'shorturl' in r:
                 return 'Failed! Not a valid URL?'
             details = r.get('url', {})
-            return '%s -> %s (%s)' % (r['shorturl'],
-                                      details.get('url'),
-                                      details.get('title'))
+            return '%s -> %s' % (r['shorturl'],
+                                 details.get('url'))
         except requests.Timeout:
             return 'Timeout'
